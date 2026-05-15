cc3c63d0e80bcd5458415f0d24f5a23724b43b3e
harshkorat09 <harshkorat106@gmail.com>
2026-05-15 18:15:01 +0530

    Fixed production API URL

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+const apiUrl = import.meta.env.VITE_API_URL
 
 const ContactForm = () => {
   const [name, setName] = useState('')
